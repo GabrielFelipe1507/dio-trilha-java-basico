923e0377c51533846aff13655d5bf5f96fe11e2f
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-01-26 19:04:34 -0300

    atualizacao 1

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.5;
+        byte idade = 123;
+        short ano = 2021;
+        int cep  = 21070333;
+        long cpf = 9878921223L;
+        float pi = 3.14F;
+        double salario = 1275.33;
     }
 }
