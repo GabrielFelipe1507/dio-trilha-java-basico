f5cee36f8fd114d441e9fec2838b0521a0a36ea7
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-01-26 20:31:32 -0300

    atualização 5

--- Tipos-variaveis/src/Operadores.java
+++ Tipos-variaveis/src/Operadores.java
@@ -37,5 +37,52 @@ public class Operadores {
 
           System.out.println("subtração:\t" + subtracao);
 
+          // estruturas condicionais:
+
+            int a, b;
+
+            a= 5;
+            b= 6;
+
+                String resultado = "";
+                    if(a==b)
+                        resultado = "verdadeiro";
+                    else{
+                        resultado = "falso";
+                    }
+            System.out.println(resultado);
+
+        // exemplo 2 com boolean:
+            int numero1 = 1;
+            int numero2 = 2;
+
+            boolean simNao = numero1 == numero2;
+            boolean simNao1 = numero2 > numero1;
+
+            System.out.println("Número 1 é Igual ao Número 2?:\t" + simNao);                    
+            System.out.println("Número 2 é Maior que o Número 2?:\t" + simNao1); 
+
+        // exemplo 3 com String e boolean:
+
+            String nomeUm = "Gabriel";
+            String nomeDois = "Gustavo";
+
+            boolean simNao2 = nomeUm == nomeDois;
+
+            System.out.println("Nome 1 é igual a Nome 2?:\t" +simNao2);
+        
+        // exemplo 4 condição verdadeira sim  ou nao ?: utilizando o "&&" e o "||" no if:
+            boolean number1 = true;
+            boolean number2 = false;
+
+            // aqui vai checar se as duas condições acima são verdadeiras:
+            if(number1 && number2){
+                System.out.println("AS DUAS SÃO VERDADEIRAS");
+            }else if(number1 || number2){
+                System.out.println("UMA SÓ É FALSA");
+            }else{
+                System.out.println("AS DUAS SÃO FALSAS");
+            }      
+
     }
 }
