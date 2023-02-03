c41860a4d3304a2449411046c33d83c1078eab18
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-02-03 19:48:26 -0300

    atualização 2

---
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //os argumentos começam com indice 0
+       /* 
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        */
+
+                //criando o objeto scanner
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+                
+                System.out.println("Digite seu nome");
+                String nome = scanner.next();
+                
+                System.out.println("Digite seu sobrenome");
+                String sobrenome = scanner.next();
+        
+                System.out.println("Digite sua idade");
+                int idade = scanner.nextInt();
+                
+                System.out.println("Digite sua altura");
+                double altura = scanner.nextDouble();
+        
+                
+                //imprimindo os dados obtidos pelo usuario
+                System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+                System.out.println("Tenho " + idade + " anos ");
+                System.out.println("Minha altura é " + altura + "cm ");                
+            
+        }        
+}
--- JAVA-TERMINAL/src/Comentarios.java
+++ JAVA-TERMINAL/src/Comentarios.java
@@ -1,13 +1,13 @@
 public class Comentarios {
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         // Olá, eu sou um comentário em uma única linha}
         /* Olá,
             * Eu sou um comentario
             * que posso ser mais detalhadod
             * quando necessário
         */      
-    }
+   // }
 
     /** 
      * Estas duas estrelinhas acima
