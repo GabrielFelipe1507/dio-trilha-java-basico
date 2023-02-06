addb7a640fb7e0489b6426a8fc7e754c3191d6ac
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-02-06 20:15:37 -0300

    atualização 2

--- UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Main.java
+++ UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Main.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        Scanner leitura = new Scanner(System.in);
+
+        //Calculadora:
+        System.out.println("Calculadora:");
         Calculadora.soma(2, 4);
         Calculadora.subtracao(4, 5);
         Calculadora.multiplicacao(3, 3);
         Calculadora.divisao(2, 1);
+
+        //Mensagem:
+        int hora;
+
+        System.out.println("Mensagem:");
+        
+        do{
+        System.out.println("Você pode ganhar um Bom Dia, Boa Tarde ou Boa Noite");
+        System.out.println("Digite um Horário do Dia:\t");
+        hora = leitura.nextInt();
+
+        Mensagem.obterMensagem(hora);
+
+        }while(hora >= 0 && hora <=24);
+        
+        //Empréstimo:
+        System.out.println("Empréstimo:");
+
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 10);
+
     }
 }
--- UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Mensagem.java
+++ UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Mensagem.java
@@ -1,3 +1,17 @@
 public class Mensagem {
     
+    //aqui neste método mostrará uma mensagem de acordo com a hora digitada na classe main
+    public static void obterMensagem(int hora){
+
+        if(hora >=0 && hora<13){
+            System.out.println("Bom Dia");
+        }else if(hora>=13 && hora <19){
+            System.out.println("Boa Tarde");
+        }else if(hora>=19 && hora <=24){
+            System.out.println("Boa Noite");
+        }else{
+            System.out.println("******** Digite uma Hora Válida ******** - Formato: 0 às 24 horas");
+        }
+    }
 }
+
