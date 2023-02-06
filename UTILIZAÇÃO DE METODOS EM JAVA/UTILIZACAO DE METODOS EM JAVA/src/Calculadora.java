ba8470083c5ccf2e0b19744bbad6657acb359a9d
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-02-06 19:33:19 -0300

    atualização 1

---
+++ UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Calculadora.java
@@ -0,0 +1,44 @@
+public class Calculadora {
+    
+    public static void soma(double num1, double num2){
+
+        double soma;
+
+        soma = num1 + num2;
+
+        System.out.println("Resultado da Soma:\t" + soma);
+
+    }
+
+    public static void subtracao(double num1, double num2){
+
+        double subtracao;
+
+        subtracao = num1 - num2;
+
+        System.out.println("Resultado da Subtração:\t" + subtracao);
+
+    }
+
+    public static void multiplicacao(double num1, double num2){
+
+        double multiplicacao;
+
+        multiplicacao = num1 * num2;
+
+        System.out.println("Resultado da Multiplicacao:\t" + multiplicacao);
+
+    }
+
+    public static void divisao(double num1, double num2){
+
+        double divisao;
+
+        divisao = num1/num2;
+
+        System.out.println("Resultado da Divisao:\t" + divisao);
+
+    }
+
+
+}
---
+++ UTILIZAÇÃO DE METODOS EM JAVA/UTILIZACAO DE METODOS EM JAVA/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Calculadora.soma(2, 4);
+        Calculadora.subtracao(4, 5);
+        Calculadora.multiplicacao(3, 3);
+        Calculadora.divisao(2, 1);
+    }
+}
